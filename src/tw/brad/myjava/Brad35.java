5c571732d8f779914c98ac61f101870947ce4f45
danic <danic@LAPTOP-5DP4MJ75.r1803.tciii.org.tw>
2022-10-20 10:06:29 +0800

    202210201006 抽象方法當場實作 Bike

--- src/tw/brad/myjava/Brad35.java
+++ src/tw/brad/myjava/Brad35.java
@@ -2,6 +2,8 @@
 
 package tw.brad.myjava;
 
+import tw.brad.myclass.Bike;
+
 public class Brad35 {
 
 	public static void main(String[] args) {
@@ -23,7 +25,16 @@ public class Brad35 {
 				
 			}
 		});
-		}
+		
+		Bike b1 = new Bike() { // 當場實作一個客製化的 ex.腳踏車，當場override
+			@Override
+			public void upSpeed() {
+				// TODO Auto-generated method stub
+				super.upSpeed();
+			}
+		};
+		
+	}
 	static void m1(Brad351 obj) {
 		
 	}
