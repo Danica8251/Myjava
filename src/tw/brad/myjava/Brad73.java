7f18041c3239a9418a8452ff2514dfeb563ba4d9
danic <danic@LAPTOP-5DP4MJ75.r1803.tciii.org.tw>
2022-10-26 16:37:22 +0800

    202210261637

--- src/tw/brad/myjava/Brad73.java
+++ src/tw/brad/myjava/Brad73.java
@@ -1,11 +1,39 @@
 package tw.brad.myjava;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Brad73 {
 
 	public static void main(String[] args) {
 		
+		Timer timer = new Timer();
+		MyTask task1 = new MyTask();
+		timer.schedule(task1, 1000, 500);
+		EndTask task2 = new EndTask(timer);
+		timer.schedule(task2, 10*1000);
+		System.out.println("OK");
 		
-		
+	}
+	
+	static class MyTask extends TimerTask{ // 內部類別
+		int i;
+		@Override
+		public void run() {
+			System.out.println(i++);
+		}
+	}
+	
+	static class EndTask extends TimerTask{
+		private Timer timer;
+		EndTask(Timer timer){
+			this.timer = timer;
+		}
+		@Override
+		public void run() {
+			timer.cancel();
+			timer.purge();
+		}
 	}
 
 }
--- src/tw/brad/myjava/MySign.java
+++ src/tw/brad/myjava/MySign.java
@@ -1,85 +1,137 @@
-///*
-// * 簽名程式
-// * Brad01 - 42 總複習
-// * */
-//
-//package tw.brad.myjava;
-//
-//import java.awt.*;
-//import java.awt.event.*;
-//
-//import javax.swing.*;
-//
-//import tw.brad.myclass.MyDrawer;
-//
-//public class MySign extends JFrame {
-//	
-//	private JButton clear, undo, redo, save, saveas, load; // 2-1 先key此行再key下面
-//	private JPanel myDrawer; // 3-1 先key此行再key下面
-//	
-//	public MySign() {
-//		super("簽名App");
-//		
-//		// 2.視窗版面配置
-//		setLayout(new BorderLayout());
-//		
-//		// 2-1. 設計按鈕
-//		clear = new JButton("Clear");
-//		undo = new JButton("Undo");
-//		redo = new JButton("Redo");
-//		
-//		// 2-2.配置按鈕位置
-//		JPanel top = new JPanel(new FlowLayout());
-//		top.add(clear); top.add(undo); top.add(redo);
-//		
-//		add(top, BorderLayout.NORTH);
-//		
-//		// 3. 至package tw.brad.myclass 新增 MyDrawer 新類別
-//		
-//		// 3-1
-//		myDrawer = new MyDrawer();
-//		add(myDrawer, BorderLayout.CENTER);
-//		
-//		
-//		// 1.視窗
-//		setSize(800,480);
-//		setVisible(true);
-//		setDefaultCloseOperation(EXIT_ON_CLOSE);
-//		
-//		// 3-16-2
-//		setupListenter();
-//	}
-//	
-//	// 3-16-1
-//	private void setupListenter() {
-//		clear.addActionListener(new ActionListener() {
-//			
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				myDrawer.clear();
-//			}
-//		});
-//		
-//		undo.addActionListener(new ActionListener() {
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				myDrawer.undo();
-//			}
-//		});
-//		
-//		redo.addActionListener(new ActionListener() {
-//			
-//			@Override
-//			public void actionPerformed(ActionEvent e) {
-//				myDrawer.redo();
-//			}
-//		});
-//	}
-//	
-//
-//
-//	public static void main(String[] args) {
-//		new MySign();
-//	}
-//
-//}
+package tw.brad.myjava;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import tw.brad.myclass.MyClock;
+import tw.brad.myclass.MyDrawer;
+
+public class MySign extends JFrame {
+	private JButton clear, undo, redo, chColor, save, saveObj, loadObj;
+	private MyDrawer myDrawer;
+	private MyClock myClock;
+	
+	public MySign() {
+		super("簽名App");
+		
+		setLayout(new BorderLayout());
+		
+		clear = new JButton("Clear");
+		undo = new JButton("Undo");
+		redo = new JButton("Redo");
+		chColor = new JButton("Color");
+		save = new JButton("Save JPG");
+		saveObj = new JButton("Save Obj");
+		loadObj = new JButton("Load Obj");
+		myClock = new MyClock();
+		
+		JPanel top = new JPanel(new FlowLayout());
+		top.add(clear); top.add(undo); top.add(redo);
+		top.add(chColor); top.add(save);
+		top.add(saveObj); top.add(loadObj);
+		top.add(myClock);
+		
+		add(top, BorderLayout.NORTH);
+		
+		myDrawer = new MyDrawer();
+		add(myDrawer, BorderLayout.CENTER);
+		
+		setSize(800, 480);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		setupListener();
+	}
+	
+	private void setupListener() {
+		clear.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				myDrawer.clear();
+			}
+		});
+		
+		undo.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				myDrawer.undo();
+			}
+		});
+		
+		redo.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				myDrawer.redo();
+			}
+		});
+		
+		chColor.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				changeColor();
+			}
+		});
+		save.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				saveJPG();
+			}
+		});
+		
+		saveObj.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					myDrawer.saveLines();
+				}catch (Exception e2) {
+				}
+			}
+		});
+		
+		loadObj.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					myDrawer.loadLines();
+				}catch (Exception e2) {
+				}
+			}
+		});
+		
+	}
+	
+	private void saveJPG() {
+		JFileChooser jfc = new JFileChooser();
+		if (jfc.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+			File saveFile = jfc.getSelectedFile();
+			if (myDrawer.saveJpeg(saveFile)) {
+				JOptionPane.showMessageDialog(null, "Save OK");
+			}else {
+				JOptionPane.showMessageDialog(null, "Save Failure");
+			}
+		}
+	}
+	
+	private void changeColor() {
+		Color newColor = JColorChooser.showDialog(null, "Line Coloe", myDrawer.getNowColor());
+		myDrawer.setNowColor(newColor);
+	}
+	
+	public static void main(String[] args) {
+		new MySign();
+	}
+
+}
